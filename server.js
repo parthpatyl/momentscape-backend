6a801679ad29c45c7f8adeb3293f36b1518b4146
parthpatyl <parthpatyl@gmail.com>
2025-05-10 06:12:57 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' })); // Adjust if needed
+app.use(cors({ origin: 'https://momentscape-phi.vercel.app/' })); // Adjust if needed
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
