f162292c9608153daa33984d7ba5ba0c8ca5b57b
parthpatyl <parthpatyl@gmail.com>
2025-05-10 17:12:41 +0530

    BACK

--- server.js
+++ server.js
@@ -4,21 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
-const allowedOrigins = process.env.CORS_ORIGIN
-  ? process.env.CORS_ORIGIN.split(',')
-  : [];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error('Not allowed by CORS'));
-  }
-}));
+app.use(cors());
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000/' })); // Adjust if needed
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
